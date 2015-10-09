8606eeae0f556287febe31eb814adb7c6c454283
anandpatel23 <ap947@drexel.edu>
2015-10-09 09:58:16 -0400

    I actually set everything up even though I said I wouldn't

--- js/script.js
+++ js/script.js
@@ -1,23 +1,18 @@
-$('#div2').hide();
-$(document).ready(function(){
+$( document ).ready(function() {
+    $('#div2').hide();
 	$('#div1').delay(1000).toggle('slide', {direction: 'right'}, 'slow');
 	$('#div2').delay(1000).toggle('slide', {direction: 'left'}, 'slow');
-});
 
-$('#div4').hide();
-$(document).ready(function(){
+	$('#div4').hide();
 	$('#div3').delay(2000).toggle('slide', {direction: 'up'}, 'slow');
 	$('#div4').delay(2000).toggle('slide', {direction: 'down'}, 'slow');
-});
 
-$('#div6').hide();
-$(document).ready(function(){
+	$('#div6').hide();
 	$('#div5').delay(3000).toggle('slide', {direction: 'left'}, 'slow');
 	$('#div6').delay(3000).toggle('slide', {direction: 'right'}, 'slow');
-});
 
-$('#div8').hide();
-$(document).ready(function(){
+
+	$('#div8').hide();
 	$('#div7').delay(4000).toggle('slide', {direction: 'down'}, 'slow');
 	$('#div8').delay(4000).toggle('slide', {direction: 'up'}, 'slow');
 });
